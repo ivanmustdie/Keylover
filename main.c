af2477a26419bea3f4565848c807f5a51d4d450e
ivanmustdie <h3005972@mvrht.com>
2016-10-17 19:31:57 +0200

    Added key capture for letter "a"

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
 
 int main()
 {
     printf("Hello world!\n");
+    while(!(GetAsyncKeyState(65)& 0x8000 != 0)){
+
+    }
+    printf("You pressed A");
+    getchar();
     return 0;
 }
